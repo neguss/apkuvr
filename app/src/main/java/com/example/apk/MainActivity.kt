245c01f856f3023c74ffdc67adf0fb1e24e2159f
neg <personalnegus@gmail.com>
2022-09-01 14:28:52 +0300

    first time

--- app/src/main/java/com/example/apk/MainActivity.kt
+++ app/src/main/java/com/example/apk/MainActivity.kt
@@ -931,7 +931,7 @@ class MainActivity : AppCompatActivity() {
             db.addRowPers("Чурилов Э.В.", "PERSON")
         }
         //Заполняем объекты
-        var tmp=db.getObject()
+        var tmp=db.getObject()//ew
         if (tmp!=null) {
             while (tmp.moveToNext()) {
                 arr+=tmp.getString(tmp.getColumnIndex("OBJ_NAME"))
