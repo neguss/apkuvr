07e0f3ba082b02cedf670609998f4aff817056b3
neguss <54805737+neguss@users.noreply.github.com>
2022-09-11 17:47:32 +0400

    result table works)

--- app/src/main/java/com/example/apk/Result.kt
+++ app/src/main/java/com/example/apk/Result.kt
@@ -35,6 +35,7 @@ class Result : AppCompatActivity() {
         }
     }
     private fun addrow(name:String,result:String){
+        /*
         val tabcol1=findViewById<LinearLayout>(R.id.col1)
         val tabcol2=findViewById<LinearLayout>(R.id.col2)
         val inflater=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -48,5 +49,23 @@ class Result : AppCompatActivity() {
         val txt2= findViewById<TextView>(textfield2.id)
         txt1.text=name
         txt2.text=result
+        */
+        val tab=findViewById<LinearLayout>(R.id.tab)
+        val inflater=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val textfield:View= inflater.inflate(R.layout.text_row,null)
+        tab.addView(textfield,tab.childCount-1)
+        textfield.id=View.generateViewId()
+        val cont=findViewById<LinearLayout>(textfield.id)
+        val tmp1= cont.getChildAt(cont.childCount-2)
+        val tmp2= cont.getChildAt(cont.childCount-1)
+        tmp2.textAlignment=View.TEXT_ALIGNMENT_GRAVITY
+        tmp1.id=View.generateViewId()
+        tmp2.id=View.generateViewId()
+        val txt1=findViewById<TextView>(tmp1.id)
+        val txt2=findViewById<TextView>(tmp2.id)
+        txt1.text=name
+        txt2.text=result
+
+
     }
 }
